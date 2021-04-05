51cd195625ddc995d012254b04458aabfc4f7456
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-04-05 12:37:27 +0600

    classes/teacher.js - parseTimeSlots : Modified

--- algorithm/classes/teacher.js
+++ algorithm/classes/teacher.js
@@ -1,39 +1,18 @@
 class Teacher {
-    constructor(id, name, designation, courses, timeSlots) {
+    constructor(id, name, designation, courses, timeSlotsInput) {
         this.id = id;
         this.name = name;
         this.designation = designation;
         this.courses = (courses) ? courses.split(";") : []; 
-        this.timeSlots = this.formatTimeSlots(timeSlots);
+        this.timeSlots = this.parseTimeSlots(timeSlotsInput);
     }
 
-    formatTimeSlots(timeSlots) {
-        for (var day of Object.keys(timeSlots)) {
-            let dayTimeSlots = timeSlots[day].split(";");
+    parseTimeSlots(timeSlotsInput) {
+        var timeSlots = [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]];
 
-            // 24 Hour Formatting            
-            // for(let i=0; i<timeSlots[day].length; i++) {
-            //     let times = dayTimeSlots[i].split("-");
+        for (var day of Object.keys(timeSlotsInput)) {
+            let dayTimeSlots = timeSlotsInput[day].split(";");
 
-            //     for(let j=0; j<2; j++) {
-            //         let time = times[j].split(":");                        
-            //         let hour = time[0];
-            //         let minute = time[1].substring(0, 2);
-            //         let pm = (time[1][2] == "p");
-            //         if(pm) { 
-            //             hour = (12+parseInt(hour)).toString();
-            //         }
-            //         times[j] = hour + ":" + minute;    
-            //     }
-
-            //     dayTimeSlots[i] = {
-            //         start: times[0],
-            //         end: times[1]
-            //     }
-            // }
-
-            // 0-1 Formatting
-            timeSlots[day] = [1, 1, 1, 1, 1];
             for(let i=0; i<dayTimeSlots.length; i++) {
                 let times = dayTimeSlots[i].split("-");                
                 // console.log(times);
@@ -90,9 +69,29 @@ class Teacher {
                     default:
                 }
 
+                let dayIndex;
+                switch(day) {
+                    case "sunday":
+                        dayIndex = 0;
+                        break;
+                    case "monday":
+                        dayIndex = 1;
+                        break;
+                    case "tuesday":
+                        dayIndex = 2;
+                        break;
+                    case "wednesday":
+                        dayIndex = 3;
+                        break;
+                    case "thursday":
+                        dayIndex = 4;
+                        break;
+                    default:
+                }
+
                 // console.log(start + " " + end);
                 for(let i=start; i<end; i++) {
-                    timeSlots[day][i] = 0;
+                    timeSlots[dayIndex][i] = 0;
                 }
             }
         }
