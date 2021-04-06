c229f3964bea952e059a5cf659c85af4065c8015
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-04-07 00:53:20 +0600

    Output Done: One Period Per Week

--- algorithm/scheduler.js
+++ algorithm/scheduler.js
@@ -6,23 +6,30 @@ const Output = require('./utils/output');
 const Session = require('./classes/session');
 const Batch = require('./classes/batch');
 const Teacher = require('./classes/teacher');
+const Routine = require('./classes/routine');
 
 var sessions = [];
 var timeSlots = [];
 var teachers = [];
 var batches = {};
 
-var routine = {
-    1 : [ [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1] ],
-    2 : [ [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1] ],
-    3 : [ [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1] ],
-}
+// var routine = {
+//     1 : [ [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1] ],
+//     2 : [ [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1] ],
+//     3 : [ [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1] ],
+// }
+
+var routines = {1 : new Routine(1), 2 : new Routine(2), 3 : new Routine(3)};
 
 /* Functions */
 
 function solve(index) {
     if(index == sessions.length) {
         return true;
+    }
+
+    if(sessions[index].count == 0) {
+        return solve(index+1);
     }
 
     let isValidDecision = false;
@@ -51,8 +58,10 @@ function solve(index) {
                                 teacher2.occupyTimeSlot(day, slot);
                                 teacher2.occupyTimeSlot(day, slot+1);
                             }
-                            routine[session.batch][day][slot] = index;
-                            routine[session.batch][day][slot+1] = index;
+                            // routine[session.batch][day][slot] = index;
+                            // routine[session.batch][day][slot+1] = index;
+                            routines[session.batch].setSession(day, slot, index);
+                            routines[session.batch].setSession(day, slot+1, index);
 
                             isValidDecision = solve(index+1);
 
@@ -67,8 +76,13 @@ function solve(index) {
                                     teacher2.freeTimeSlot(day, slot);
                                     teacher2.freeTimeSlot(day, slot+1);
                                 }
-                                routine[session.batch][day][slot] = -1;
-                                routine[session.batch][day][slot+1] = -1;
+                                // routine[session.batch][day][slot] = -1;
+                                // routine[session.batch][day][slot+1] = -1;
+                                routines[session.batch].removeSession(day, slot, index);
+                                routines[session.batch].removeSession(day, slot+1, index);
+                            }
+                            else {
+                                sessions[index].decrementCount();
                             }
                         }
                     }
@@ -87,13 +101,16 @@ function solve(index) {
                 if(teacher.isTimeSlotFree(day, slot) && batch.isTimeSlotFree(group, day, slot)) {
                     batch.occupyTimeSlot(group, day, slot);
                     teacher.occupyTimeSlot(day, slot);
-                    routine[session.batch][day][slot] = index;
+                    // routine[session.batch][day][slot] = index;
+                    routines[session.batch].setSession(day, slot, index);
+
                     isValidDecision = solve(index+1);
 
                     if(!isValidDecision) {
                         batch.freeTimeSlot(group, day, slot);
                         teacher.freeTimeSlot(day, slot);
-                        routine[session.batch][day][slot] = -1;
+                        // routine[session.batch][day][slot] = -1;
+                        routines[session.batch].removeSession(day, slot, index);
                     }
                 }
             }
@@ -107,10 +124,11 @@ function main() {
     Input.load('./data/input.xlsx', sessions, timeSlots, teachers, batches);
     // Input.show(sessions, timeSlots, teachers, batches);
 
-    let isPossible = solve(0);
+    var isPossible = solve(0);
 
-    console.log(isPossible);
-    Output.showRoutine(routine, sessions);
+    // console.log(isPossible);
+    Output.showRoutines(routines, sessions);
+    // Output.debugRoutines(routines);
 }
 
 /* Executions */
--- algorithm/utils/output.js
+++ algorithm/utils/output.js
@@ -1,25 +1,92 @@
-function showRoutine(routine, sessions) {
+const Table = require('table');
+const Routine = require('./../classes/routine');
+
+function showRoutines(routines, sessions) {
+    
+    var config = {
+        columns: {
+            0: {
+                alignment: 'left',
+                width: 10
+            },
+            1: {
+                alignment: 'center',
+                width: 15
+            },
+            2: {
+                alignment: 'center',
+                width: 15
+            },
+            3: {
+                alignment: 'center',
+                width: 15
+            },
+            4: {
+                alignment: 'center',
+                width: 15
+            },
+            5: {
+                alignment: 'center',
+                width: 15
+            }
+        }
+    };
+
+    var data, output;
     let yearNames = ["First", "Second", "Third", "Fourth"];
-    let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
+
     console.log("CSEDU Routines");
     console.log();
     for(let batch=1; batch<=3; batch++) {
         console.log(yearNames[batch-1] + " Year Routine");
+
+        data = [
+            ['Day/Time', '08:30 AM', '10:00 AM', '11:30 AM', '02:00 PM', '03:30 PM'],
+            ['Sunday', '', '', '', '', ''],
+            ['Monday', '', '', '', '', ''],
+            ['Tuesday', '', '', '', '', ''],
+            ['Wednesday', '', '', '', '', ''],
+            ['Thursday', '', '', '', '', '']
+        ];
+
         for(let day=0; day<5; day++) {
-            process.stdout.write(dayNames[day] + ": ");
             for(let slot=0; slot<5; slot++) {
-                if(routine[1][day][slot]!=-1) {
-                    process.stdout.write(sessions[routine[1][day][slot]].course + " ");
-                }
-                else{
-                    process.stdout.write("None ");
+                var sessionIndices = routines[batch].getSessions(day, slot);
+                if(sessionIndices.length>0) {
+                    sessionIndices.forEach(index => {
+                        let session = sessions[index];
+                        let sessionName = session.course;
+                        if(session.type == "Lab") {
+                            sessionName += " " + session.group;
+                        }
+                        if(data[day+1][slot+1].length>0) {
+                            data[day+1][slot+1] += '\n';
+                        }
+                        data[day+1][slot+1] += sessionName;
+                    });
                 }
             }
-            console.log();
         }
-        console.log();
-        console.log();
+
+        output = Table.table(data, config);
+        console.log(output);
     }
 }
 
-module.exports.showRoutine = showRoutine;
+function debugRoutines(routines) {
+    // routines[1].setSession(0, 1, 0);
+    // routines[1].setSession(0, 2, 0);
+    // routines[1].setSession(0, 3, 1);
+    // routines[1].setSession(0, 4, 1);
+    // routines[1].setSession(1, 1, 2);
+    // routines[1].setSession(1, 2, 2);    
+
+    // console.log(routines[1].states);
+    Object.keys(routines).forEach((batch)=>{ 
+        console.log(routines[batch].states);
+    });
+    // console.log(routines);
+}
+
+module.exports.showRoutines = showRoutines;
+module.exports.debugRoutines = debugRoutines;
