6895aa679e6c7135c3537e6ebe75c52099b20e03
S.M.Mehrab <mehrab.24csedu.001@gmail.com>
2021-04-07 00:50:37 +0600

    classes/routine.js - setSession, removeSession : Modified

--- algorithm/classes/routine.js
+++ algorithm/classes/routine.js
@@ -73,39 +73,19 @@ class Routine {
     }
 
     setSession(dayIndex, slotIndex, sessionIndex) {
-        let dayCount = 0;
-        Object.keys(this.states).forEach((day)=>{
-            if(dayCount == dayIndex) {
-                let slotCount = 0;
-                Object.keys(this.states[day]).forEach((slot)=>{ 
-                    if(slotCount == slotIndex) {
-                        this.states[day][slot].push(sessionIndex);    
-                    }
-                    slotCount++;
-                });
-            }
-            dayCount++;
-        });
+        let day = this._toDay(dayIndex);
+        let slot = this._toSlot(slotIndex);
+        this.states[day][slot].push(sessionIndex);    
     }
 
     removeSession(dayIndex, slotIndex, sessionIndex) {
-        let dayCount = 0;
-        Object.keys(this.states).forEach((day)=>{
-            if(dayCount == dayIndex) {
-                let slotCount = 0;
-                Object.keys(this.states[day]).forEach((slot)=>{ 
-                    if(slotCount == slotIndex) {
-                        for(let index=0; index<this.states[day][slot].length; index++){ 
-                            if (this.states[day][slot][index] == sessionIndex) { 
-                                this.states[day][slot].splice(index, 1); 
-                            }
-                        }                
-                    }
-                    slotCount++;
-                });
+        let day = this._toDay(dayIndex);
+        let slot = this._toSlot(slotIndex);
+        for(let index=0; index<this.states[day][slot].length; index++){ 
+            if (this.states[day][slot][index] == sessionIndex) { 
+                this.states[day][slot].splice(index, 1); 
             }
-            dayCount++;
-        });
+        }         
     }
 
     getSessions(dayIndex, slotIndex) {
